7b7c2d34055375f409311bd0f5cef2a37615526b
ahmedgamaall <ahmedgamalahmed231@gmail.com>
2024-03-23 04:42:46 +0300

    0 - 2

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,12 @@
+#!/usr/bin/pup
 # Using Puppet, install flask from pip3
 
 package { 'flask':
 ensure   => '2.1.0',
+provider => 'pip3'
+}
+
+package { 'Werkzeug':
+ensure   => '2.1.1',
 provider => 'pip3'
 }

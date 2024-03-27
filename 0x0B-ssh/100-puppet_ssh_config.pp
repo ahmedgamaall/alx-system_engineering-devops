065cb1dcdc046a15037a8f5b90c1addd9ad620af
ahmedgamaall <ahmedgamalahmed231@gmail.com>
2024-03-26 18:15:07 -0700

    edit & update

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,6 +1,4 @@
 r/bin/env bash
-#use puppet to make changes to my config files
-#this script is to be run on the puppet master
 file { '/etc/ssh/ssh_config':
         ensure => present,
 content => "

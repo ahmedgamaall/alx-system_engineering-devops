a434b9e071e535b33be2df289a9be81d753176f7
ahmedgamaall <ahmedgamalahmed231@gmail.com>
2024-03-27 01:49:02 -0700

    100

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,9 +1,12 @@
-#!/usr/bin/env bash
+#!usr/bin/env bash
+
 file { '/etc/ssh/ssh_config':
         ensure => present,
 }
-file_line { 'Turn off password auth':
-  path    => '/etc/ssh/ssh_config',
-  line    => 'passwordAuthentication no',
-  match   => '^#passwordAuthentication',
+file_line { 'SSH Private Key':
+  path               => '/etc/ssh/ssh_config',
+  line               => '    IdentityFile ~/.ssh/school',
+  match              => '^[#]+[\s]*(?i)IdentityFile[\s]+~/.ssh/id_rsa$',
+  replace            => true,
+  append_on_no_match => true
 }

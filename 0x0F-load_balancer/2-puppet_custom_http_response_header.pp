53523b0c0143299475c01a1736cab67040ffa753
ahmedgamaall <ahmedgamalahmed231@gmail.com>
2024-04-01 07:48:12 -0700

    task 0 -2

---
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -0,0 +1,44 @@
+package{ 'nginx':
+  ensure => 'installed',
+}
+
+service{ 'nginx':
+  ensure => 'running',
+  enable => 'true',
+}
+
+file{'/etc/nginx/sites-available/default':
+  ensure  => 'file',
+  content => "server {
+    listen 80 default_server;
+    listen [::]:80 default_server ipv6only=on;
+    add_header X-Served-By ${hostname};
+    root /var/www/html;
+    index index.html index.htm;
+    server_name localhost;
+    error_page 404 /404.html;
+    location / {
+        try_files \$uri \$uri/ =404;
+    }
+
+    location /redirect_me {
+        return 301 https://www.youtube.com/watch?v=QH2-TGUlwu4;
+    }
+}",
+  require => Package['nginx'],
+  notify  => Service['nginx'],
+  replace => 'true',
+}
+
+file {'/var/www/html/index.html':
+  ensure  => 'file',
+  content => 'Hello World!',
+  require => Package['nginx'],
+  notify  => Service['nginx'],
+  }
+
+file {'/var/www/html/404.html':
+  ensure  => 'file',
+  content => "Ceci n'est pas une page",
+  require => Package['nginx'],
+  notify  => Service['nginx'],}

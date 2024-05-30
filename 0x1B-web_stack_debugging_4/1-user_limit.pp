179238abc97c63e9ccbc2a9b368056a3338a362e
ahmedgamaall <ahmedgamalahmed231@gmail.com>
2024-05-30 20:53:04 +0300

    edit

--- 0x1B-web_stack_debugging_4/1-user_limit.pp
+++
@@ -1,9 +0,0 @@
-# web stack debugging task: Change the OS configuration
-
-exec { 'holberton hard limit':
-    command => '/bin/sed -i "s/5/4096/g" /etc/security/limits.conf',
-}
-
-exec { 'holberton soft limit':
-    command => '/bin/sed -i "s/4/4096/g" /etc/security/limits.conf',
-}
